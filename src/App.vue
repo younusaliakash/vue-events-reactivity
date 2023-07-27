b41694ba47ede552d88d4aa4bfc8981553f4889b
younusaliakash <younusalikash8@gmail.com>
2023-07-27 19:09:44 +0600

    on-off reactivity done

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <script setup>
+import { reactive } from "vue";
 
+const lightIsOn = "//img.icons8.com/?size=512&id=12244&format=png";
+const lightIsOff = "//img.icons8.com/?size=512&id=75&format=png";
+
+const lightData = reactive([
+  {
+    title: "Light 1",
+    isOn: false,
+  },
+  {
+    title: "Light 2",
+    isOn: false,
+  },
+  {
+    title: "Light 3",
+    isOn: true,
+  },
+]);
 </script>
 
 <template>
   <section class="container mx-auto flex items-center flex-col">
-        <h1 class="text-center text-2xl py-10">Assignment</h1>
-        <p class="p-5">Make These Buttons Work</p>
-        <div class="flex justify-between space-x-6">
-            <div class="p-10 border w-96 flex flex-col space-y-5">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Light 1 On
-                </button>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Light 2 On
-                </button>
-                <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-                    Light 3 Off
-                </button>
-            </div>
-            <div class="p-10 border w-96 flex flex-col space-y-5">
-                <img class="w-20" src="//img.icons8.com/?size=512&id=12244&format=png" alt="">
-                <img class="w-20" src="//img.icons8.com/?size=512&id=12244&format=png" alt="">
-                <img class="w-20" src="//img.icons8.com/?size=512&id=75&format=png" alt="">
-            </div>
-        </div>
-    </section>
+    <h1 class="text-center text-2xl py-10">Assignment</h1>
+    <p class="p-5">Make These Buttons Work</p>
+    <div class="flex justify-between space-x-6">
+      <div class="p-10 border w-96 flex flex-col space-y-5">
+        <button
+          v-for="light of lightData"
+          :key="light.title"
+          class="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          :class="light.isOn ? 'bg-blue-500' : 'bg-black'"
+          @click="light.isOn = !light.isOn"
+        >
+          {{ light.title }} {{ light.isOn ? "On" : "Off" }}
+        </button>
+      </div>
+      <div class="p-10 border w-96 flex flex-col space-y-5">
+        <img
+          v-for="light of lightData"
+          :key="light.title"
+          class="w-20"
+          :src="light.isOn ? lightIsOn : lightIsOff"
+          :alt="light.title"
+        />
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
