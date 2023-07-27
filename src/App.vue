0a3cc0b4e2305aeab54d8029982e72e3990c6581
younusaliakash <younusalikash8@gmail.com>
2023-07-27 19:27:33 +0600

    pp6 done

--- src/App.vue
+++ src/App.vue
@@ -1,127 +1,52 @@
 <script setup>
 import { ref, reactive } from "vue";
-//reactivity
-const activeIndex = ref(0);
-const accordions = reactive([
-  {
-    heading: "What is term?",
-    content:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi dolor dolorum odio, saepe quibusdam iusto possimus nesciunt dolores assumenda quae totam, doloremque odit. Itaque cum animi, labore debitis deserunt iusto!",
-    isOpen: true,
-  },
-  {
-    heading: "When to use Accordion Components?",
-    content:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut suscipit itaque, sequi incidunt ab sit optio debitis dignissimos doloribus ratione nobis id mollitia maiores eveniet necessitatibus dolorum praesentium! Corrupti, at.",
-    isOpen: false,
-  },
-  {
-    heading: "How can it be defined?",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima facilis labore voluptates provident nam, delectus suscipit nihil optio voluptate quae porro fugiat magni excepturi doloribus.",
-    isOpen: false,
-  },
-  {
-    heading: "Chamber reached do he nothing be?",
-    content:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam minima maiores magni dolore qui architecto pariatur incidunt repellat quos hic? Eius, eveniet accusantium! Quisquam, quo. Vitae ipsum ad veritatis commodi quasi expedita!",
-    isOpen: false,
-  },
-]);
 
-// accordions[0].isOpen=true
+const fullImage = ref(
+  "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
+);
+const images = [
+  {
+    thumb:
+      "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
+  },
+  {
+    thumb:
+      "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
+  },
+  {
+    thumb:
+      "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
+  },
+  {
+    thumb:
+      "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
+  },
+];
+
+function setImage(image) {
+  fullImage.value = image.full;
+}
 </script>
 
 <template>
   <section class="container mx-auto flex items-center flex-col">
-    <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
-
-    <div
-      class="w-full p-10 bg-gradient-to-br from-pink-50 to-indigo-100 grid place-items-center"
-    >
-      <div class="w-3/4 mx-auto rounded border">
-        <div class="bg-white p-10 shadow-sm">
-          <h3 class="text-lg font-medium text-gray-800">
-            Several Windows stacked on each other
-          </h3>
-          <p class="text-sm font-light text-gray-600 my-3">
-            The accordion is a graphical control element comprising a vertically
-            stacked list of items such as labels or thumbnails
-          </p>
-
-          <div class="h-1 w-full mx-auto border-b my-5"></div>
-
-          <!-- What is term -->
-          <div
-            class="transition hover:bg-indigo-50"
-            v-for="(accordion, index) in accordions"
-            :key="index"
-          >
-            <!-- header -->
-            <div
-              @click="activeIndex = index"
-              class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16"
-            >
-              <i
-                class="fas"
-                :class="index == activeIndex ? 'fa-minus' : 'fa-plus'"
-              ></i>
-              <h3>{{ accordion.heading }}</h3>
-            </div>
-            <!-- Content -->
-            <div class="px-5 pt-0 text-left pb-5" v-show="index == activeIndex">
-              <p class="leading-6 font-light pl-9">
-                {{ accordion.content }}
-              </p>
-            </div>
-          </div>
-        </div>
+    <h1 class="text-center text-2xl py-10">Events & Reactivity</h1>
+    <div class="">
+      <div class="flex space-x-2">
+        <img
+          @click="setImage(image)"
+          class="cursor-pointer"
+          v-for="image in images"
+          :src="image.thumb"
+          :key="image.thumb"
+        />
       </div>
-    </div>
-
-    <div
-      class="w-full p-10 bg-gradient-to-br from-pink-50 to-indigo-100 grid place-items-center"
-    >
-      <div class="w-3/4 mx-auto rounded border">
-        <div class="bg-white p-10 shadow-sm">
-          <h3 class="text-lg font-medium text-gray-800">
-            Several Windows stacked on each other
-          </h3>
-          <p class="text-sm font-light text-gray-600 my-3">
-            The accordion is a graphical control element comprising a vertically
-            stacked list of items such as labels or thumbnails
-          </p>
-
-          <div class="h-1 w-full mx-auto border-b my-5"></div>
-
-          <!-- What is term -->
-          <div
-            class="transition hover:bg-indigo-50"
-            v-for="(accordion, index) in accordions"
-            :key="index"
-          >
-            <!-- header -->
-            <div
-              @click="accordion.isOpen = !accordion.isOpen"
-              class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16"
-            >
-              <i
-                class="fas"
-                :class="accordion.isOpen ? 'fa-minus' : 'fa-plus'"
-              ></i>
-              <h3>{{ accordion.heading }}</h3>
-            </div>
-            <!-- Content -->
-            <div
-              class="px-5 pt-0 text-left pb-5"
-              v-show="true == accordion.isOpen"
-            >
-              <p class="leading-6 font-light pl-9">
-                {{ accordion.content }}
-              </p>
-            </div>
-          </div>
-        </div>
+      <div class="mt-10">
+        <img class="cursor-pointer w-full" :src="fullImage" />
       </div>
     </div>
   </section>
