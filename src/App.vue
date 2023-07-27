73896ffc207d7d3cc95672988419d185dc991673
younusaliakash <younusalikash8@gmail.com>
2023-07-27 19:18:28 +0600

    practice done

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <script setup>
+import {ref} from 'vue'
+const location = ref("Dhaka")
 
+function changeLocation(newLocation){
+  location.value = newLocation
+}
 </script>
 
 <template>
   <section class="container mx-auto flex items-center flex-col">
-        <h1 class="text-center text-2xl py-10">Assignment</h1>
-        <p class="p-5">Make These Buttons Work</p>
-        <div class="flex justify-between space-x-6">
-            <div class="p-10 border w-96 flex flex-col space-y-5">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Light 1 On
-                </button>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Light 2 On
-                </button>
-                <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-                    Light 3 Off
-                </button>
-            </div>
-            <div class="p-10 border w-96 flex flex-col space-y-5">
-                <img class="w-20" src="//img.icons8.com/?size=512&id=12244&format=png" alt="">
-                <img class="w-20" src="//img.icons8.com/?size=512&id=12244&format=png" alt="">
-                <img class="w-20" src="//img.icons8.com/?size=512&id=75&format=png" alt="">
-            </div>
+        <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
+        <div class="container mx-auto flex space-x-5 justify-center m-5">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Dhaka')">
+                Dhaka
+            </button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Rajshahi')">
+                Rajshahi
+            </button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Khulna')">
+                Khulna
+            </button>
         </div>
+        <h1 class="text-2xl m-10">Current Location is {{ location }}</h1>
     </section>
 </template>
 
