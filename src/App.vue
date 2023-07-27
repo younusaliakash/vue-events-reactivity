945140df5dd92485f449cc0b85fe340f6fb98ea7
younusaliakash <younusalikash8@gmail.com>
2023-07-27 19:21:04 +0600

    pp2 done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <script setup>
-import {ref} from 'vue'
-const location = ref("Dhaka")
+import {ref, reactive} from 'vue'
+const location = reactive({
+  name: "Dhaka",
+  weather:"Sunny"
+})
 
-function changeLocation(newLocation){
-  location.value = newLocation
+function changeLocation(newLocation) {
+  location.name = newLocation
 }
 </script>
 
 <template>
   <section class="container mx-auto flex items-center flex-col">
-        <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
-        <div class="container mx-auto flex space-x-5 justify-center m-5">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Dhaka')">
-                Dhaka
-            </button>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Rajshahi')">
-                Rajshahi
-            </button>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Khulna')">
-                Khulna
-            </button>
-        </div>
-        <h1 class="text-2xl m-10">Current Location is {{ location }}</h1>
-    </section>
+    <h1 class="text-center text-2xl py-10">Events & Reactivity in Vue.js</h1>
+    <div class="container mx-auto flex space-x-5 justify-center m-5">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Dhaka')">
+        Dhaka
+      </button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Rajshahi')">
+        Rajshahi
+      </button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Khulna')">
+        Khulna
+      </button>
+    </div>
+    <h1 class="text-2xl m-10">Current Location is {{ location.name }}</h1>
+  </section>
 </template>
 
 <style scoped></style>
